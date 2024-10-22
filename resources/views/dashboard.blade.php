b2d0759c3a17952ee16b33eb0857e40cfe606b9c
Patrik Wendt <patrik@wendt-se.de>
2024-10-22 14:24:08 +0000

    include navigation blade in deashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -84,31 +84,7 @@
             LOGO
         </div>
         <div class="position-sticky pt-3">
-            <ul class="nav flex-column">
-                <li class="nav-item">
-                    <a class="nav-link active" href="#">
-                        <i class="fas fa-home"></i> Dashboard
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#benutzerSubmenu" data-bs-toggle="collapse" aria-expanded="false">
-                        <i class="fas fa-users"></i> Benutzer
-                    </a>
-                    <ul class="collapse nav flex-column submenu" id="benutzerSubmenu">
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Alle Benutzer</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Neuer Benutzer</a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">
-                        <i class="fas fa-cog"></i> Einstellungen
-                    </a>
-                </li>
-            </ul>
+           @include('layouts/navigation')
         </div>
     </nav>
 
