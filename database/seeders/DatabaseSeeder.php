38a84d00fd02ac2ce074eee1209f28e4beb5867a
Patrik Wendt <patrik@wendt-se.de>
2024-10-25 16:33:33 +0000

    fixed db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Database\Seeders\EmployeeSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,11 +15,12 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
         User::factory()->create([
-            'name' => 'Patrik Wendt',
+            'name' => 'Wendt',
+            'vorname' => 'Patrik',
             'email' => 'pwe@pwe.de',
             'password' => '$2y$10$Ub.ZAwHgIer4/r0h6TbAmuCVQSnTX81eUh72q17BQW9dxn8YMjWcK',
         ]);
-        
+
         $this->call([
             EmployeeSeeder::class,
         ]);
