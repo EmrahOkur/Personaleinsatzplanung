248ef97eb168d84435ff82524e27088ab427403e
Patrik Wendt <patrik@wendt-se.de>
2024-10-26 12:58:39 +0000

    added new employee btn

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -9,10 +9,13 @@
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
-                        <div class="container">
-                            <div class="row mb-4">
+                        <div class="container mt-4">
+                            <div class="row mb-4 d-flex justify-content-end">
                                 <div class="col-md-6">
                                     <input type="text" id="search" class="form-control" placeholder="Mitarbeiter suchen...">
+                                </div>
+                                <div class="col-md-6" align="right">
+                                    <button class="btn btn-primary me-3">Mitarbeiter anlegen</button>
                                 </div>
                             </div>
                             <table class="table table-striped">
