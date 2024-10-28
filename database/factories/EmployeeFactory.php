842ecf594f735e2f06bac9990d1c54ebcdd5b185
Patrik Wendt <patrik@wendt-se.de>
2024-10-28 09:03:47 +0000

    employee factory email from name

--- database/factories/EmployeeFactory.php
+++ database/factories/EmployeeFactory.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
@@ -16,13 +18,18 @@ class EmployeeFactory extends Factory
      */
     public function definition(): array
     {
+        $firstName = $this->faker->firstName();
+        $lastName = $this->faker->lastName();
+        $email = $firstName . '.' . $lastName;
+        $email = str_replace(['ä', 'ö', 'ü', 'ß'], ['ae', 'oe', 'ue', 'ss'], $email);
+
         return [
-            'first_name' => $this->faker->firstName(),
-            'last_name' => $this->faker->lastName(),
-            'email' => $this->faker->unique()->safeEmail(),
+            'first_name' => $firstName,
+            'last_name' => $lastName,
+            'email' => strtolower($email) . '@personal.de',
             'phone' => $this->faker->phoneNumber(),
             'birth_date' => $this->faker->date(),
-            'employee_number' => $this->faker->unique()->numerify('EMP####'),
+            'employee_number' => $this->faker->unique()->numerify('PNR####'),
             'hire_date' => $this->faker->date(),
             'position' => $this->faker->jobTitle(),
             'vacation_days' => 30,
