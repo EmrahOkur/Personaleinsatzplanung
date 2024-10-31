ea0e8db7139cfc906e52d049cf35548531b17099
Patrik Wendt <patrik@wendt-se.de>
2024-10-31 10:25:03 +0000

    redirect if logged in

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,14 @@ use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    if (Auth::check()) {
+        return redirect()->route('home');
+    }
+
     return view('welcome');
 });
 
