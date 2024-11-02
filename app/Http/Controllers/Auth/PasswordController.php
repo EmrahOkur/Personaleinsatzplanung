ad6b36046418d9516bac0e03625d42d93dd3cdf9
Patrik Wendt <patrik@wendt-se.de>
2024-11-02 11:45:22 +0000

    edit profile page

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -1,9 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules\Password;
@@ -13,11 +14,10 @@ class PasswordController extends Controller
     /**
      * Update the user's password.
      */
-    public function update(Request $request): RedirectResponse
+    public function updatePassword(Request $request)
     {
-        $validated = $request->validateWithBag('updatePassword', [
-            'current_password' => ['required', 'current_password'],
-            'password' => ['required', Password::defaults(), 'confirmed'],
+        $validated = $request->validate([
+            'password' => ['required', 'min:8', 'confirmed'],
         ]);
 
         $request->user()->update([
---
+++ resources/views/components/top-bar.blade.php
@@ -0,0 +1,30 @@
+<div class="top-bar">
+    <button type="button" id="sidebarCollapse" class="btn btn-light">
+        <i class="fas fa-bars"></i>                
+    </button>
+
+    <div class="float-end">
+        <div class="dropdown border rounded-1">
+            <button class="btn dropdown-toggle" type="button" id="userMenu" data-bs-toggle="dropdown" aria-expanded="false">
+                {{ Auth::user()->getFullName() }}
+            </button>
+            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userMenu">
+                <li>
+                    <a href="{{ route('profile.edit') }}" class="dropdown-item">
+                        <i class="fas fa-user-edit"></i> Benutzerdaten ändern
+                    </a>
+                </li>
+                <li><hr class="dropdown-divider"></li>
+                <li>
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <button type="submit" class="dropdown-item">
+                            <i class="fas fa-sign-out-alt"></i> Logout
+                        </button>
+                    </form>
+                </li>
+            </ul>
+        </div>
+    </div>
+    @yield('header')
+</div>
