af4912dcd578ac9cf1f788c4f00dd9a016814ab6
Patrik Wendt <patrik@wendt-se.de>
2024-11-08 15:37:32 +0000

    navigation depends on roles

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -4,17 +4,48 @@
             <i class="fas fa-home"></i> Dashboard
         </a>
     </li>
-
+    
+@if(Auth::user()->hasRole('admin'))
     <li class="nav-item">
-        <a class="nav-link {{ request()->routeIs('users') ? 'active' : '' }}" href="{{ route('users') }}">
+        <a class="nav-link {{ request()->routeIs('users') ? 'active' : '' }}" href="{{ route('home') }}">
             <i class="fas fa-users"></i> {{ __('Benutzer') }}
         </a>        
     </li>
+@endif
+
+@if(Auth::user()->hasRole('manager'))
     <li class="nav-item">
         <a class="nav-link {{ request()->routeIs('employees') ? 'active' : '' }}" href="{{ route('employees') }}">
             <i class="fas fa-users"></i> {{ __('Mitarbeiter') }}
         </a>
     </li>
+@endif
+
+@if(Auth::user()->hasRole('employee'))
+<li class="nav-item">
+    <a class="nav-link {{ request()->routeIs('employees') ? 'active' : '' }}" href="#">
+        <i class="fas fa-users"></i> {{ __('Zeiterfassung') }}
+    </a>
+</li>
+@endif
+
+@if(Auth::user()->hasRole('employee'))
+<li class="nav-item">
+    <a class="nav-link {{ request()->routeIs('employees') ? 'active' : '' }}" href="#">
+        <i class="fas fa-users"></i> {{ __('Arbeitsplan') }}
+    </a>
+</li>
+@endif
+
+@if(Auth::user()->hasRole('employee'))
+<li class="nav-item">
+    <a class="nav-link {{ request()->routeIs('employees') ? 'active' : '' }}" href="#">
+        <i class="fas fa-users"></i> {{ __('Urlaubsantrag') }}
+    </a>
+</li>  
+@endif
+
+@if(Auth::user()->hasNotRole('employee'))
     <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="settingsDropdown" role="button" data-bs-toggle="collapse" data-bs-target="#settingsSubmenu" aria-expanded="false">
             <i class="fas fa-cog"></i> Einstellungen
@@ -31,4 +62,5 @@
             </ul>
         </div>
     </li>
+@endif
 </ul>
