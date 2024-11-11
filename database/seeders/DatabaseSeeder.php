562abf97d1545e8bd84744eb417553251123ef11
Patrik Wendt <patrik@wendt-se.de>
2024-11-11 14:24:53 +0000

    added admin to seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,20 +15,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
         User::factory()->create([
             'name' => 'Wendt',
             'vorname' => 'Patrik',
             'role' => 'manager',
             'email' => 'pwe@pwe.de',
-            'password' => '$2y$10$Ub.ZAwHgIer4/r0h6TbAmuCVQSnTX81eUh72q17BQW9dxn8YMjWcK',
+            'password' => '$2y$10$Ub.ZAwHgIer4/r0h6TbAmuCVQSnTX81eUh72q17BQW9dxn8YMjWcK', // pwe123 https://onlinephp.io/password-hash
         ]);
         User::factory()->create([
             'name' => 'Albig',
             'vorname' => 'Malte',
             'role' => 'manager',
             'email' => 'alm@alm.de',
-            'password' => '$2y$10$9FCLItMjbuvEgsV745Pxwurm7cx4ldGOJjELTwnirdCPp013y/9dW',
+            'password' => '$2y$10$9FCLItMjbuvEgsV745Pxwurm7cx4ldGOJjELTwnirdCPp013y/9dW', // https://onlinephp.io/password-hash
+        ]);
+        User::factory()->create([
+            'name' => 'Admin',
+            'vorname' => 'Admin',
+            'role' => 'admin',
+            'email' => 'admin@admin.de',
+            'password' => '$2y$10$rO1u4wYJRgkbGyzNkHUvt.HIZGcvGorob43XeHBWGCJyfC68Qygou', // admin123 https://onlinephp.io/password-hash
         ]);
 
         $this->call([
