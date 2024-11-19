45f8b4e22ed6f6a39a1349786cdebcb9c429e5b5
Patrik Wendt <patrik@wendt-se.de>
2024-11-19 19:19:07 +0000

    next try bootstrap fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Personalplanung')</title>
     <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/bootstrap.min.css', 'resources/css/app.css', 'resources/js/app.js', 'resources/js/bootstrap.bundle.min.js'])
     @stack('scripts')
 </head>
 <body>
