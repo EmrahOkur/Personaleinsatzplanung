2876e437a9b388c1a106f0879dd7aa3fc73e1907
Patrik Wendt <patrik@wendt-se.de>
2024-12-04 13:03:18 +0000

    customer factory

--- database/migrations/2024_10_23_073641_customer.php
+++ database/migrations/2024_10_23_073641_customer.php
@@ -1,22 +1,29 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        //
+
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
+            $table->string('companyname');
             $table->string('vorname');
             $table->string('nachname');
-            $table->string('ort');
+            $table->string('street');
+            $table->string('house_number');
+            $table->string('zip_code');
+            $table->string('city');
+            $table->string('customer_number')->unique();
+            $table->timestamps();
         });
 
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             ResponsibilitySeeder::class,
             UrlaubSeeder::class,
+            CustomerSeeder::class
         ]);
     }
 }
